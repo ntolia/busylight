18f29221f7af0bef3035b6ccd66122c878765200
Niraj Tolia <ntolia@users.noreply.github.com>
2022-04-10 14:19:28 -0700

    Detect Zoom session

---
+++ zoom/detect_zoom.py
@@ -0,0 +1,17 @@
+import sys
+import subprocess
+
+def detect_zoom():
+    proc1 = subprocess.Popen(["ps", "x"], stdout=subprocess.PIPE)
+    proc2 = subprocess.Popen(["grep", "-E", "\-key [0-9]{9,10}"], stdin=proc1.stdout, stdout=subprocess.PIPE)
+    proc1.stdout.close()
+
+    output = proc2.communicate()[0]
+    if output:
+        print("Zoom in session", output)
+    
+    else:
+        print("Zoom not in session ")
+
+if __name__ == "__main__":
+    detect_zoom()
