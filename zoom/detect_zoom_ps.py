984b096d195b70a65e22577bff8828d135ee6d92
Niraj Tolia <ntolia@users.noreply.github.com>
2022-04-11 19:41:04 -0700

    Convert ps-based Zoom code to a class

--- zoom/detect_zoom_ps.py
+++ zoom/detect_zoom_ps.py
@@ -1,18 +1,21 @@
 import sys
 import subprocess
 
-def detect_zoom():
-    proc1 = subprocess.Popen(["ps", "x"], stdout=subprocess.PIPE)
-    proc2 = subprocess.Popen(["grep", "-E", "\-key [0-9]{9,10}"], stdin=proc1.stdout, stdout=subprocess.PIPE)
-    proc1.stdout.close()
+class DetectZoom:
 
-    output = proc2.communicate()[0]
-    if output:
-        print("Zoom in session", output)
-        return true
-    else:
-        print("Zoom not in session ")
-        return false
+    def detect(self):
+        proc1 = subprocess.Popen(["ps", "x"], stdout=subprocess.PIPE)
+        proc2 = subprocess.Popen(["grep", "-E", "\-key [0-9]{9,10}"], stdin=proc1.stdout, stdout=subprocess.PIPE)
+        proc1.stdout.close()
+
+        output = proc2.communicate()[0]
+        if output:
+            print("Zoom in session", output)
+            return True
+        else:
+            print("Zoom not in session ")
+            return False
 
 if __name__ == "__main__":
-    detect_zoom()
+    dz = DetectZoom()
+    print("Zoom running?", dz.detect())
