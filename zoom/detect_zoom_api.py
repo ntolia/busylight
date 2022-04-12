ae7fbcb2bbcfc64ea179a7627e2794b845452af1
Niraj Tolia <ntolia@users.noreply.github.com>
2022-04-11 20:15:36 -0700

    Update API-based Zoom detection code

--- zoom/detect_zoom_api.py
+++ zoom/detect_zoom_api.py
@@ -2,17 +2,22 @@ import argparse
 import json
 from zoomus import ZoomClient
 
-def authenticate_and_query(api_key, api_secret):
-    client = ZoomClient(api_key, api_secret)
-    # Unfortuantely, this user_id pulls in all meetings when run with
-    # admin keys
-    user_id='me'
-    meetings = client.meeting.list(user_id=user_id, type="upcoming_meetings")
-    for meeting in meetings:
-        print(meeting)
-    users = client.user.list()
-    #for user in users:
-     #   print(user)
+class DetectZoomAPI:
+
+    def __init__(self, api_key, api_secret):
+        self.api_key = api_key
+        self.api_secret = api_secret
+
+    def detect(self):
+        client = ZoomClient(api_key, api_secret)
+        # Unfortuantely, the 'me' user_id pulls in all meetings for all users
+        # when run with admin keys. One can list users and then filter out the
+        # user one is interested in. However, the JWT token-based also cannot
+        # be scoped and so this path was abandonded.
+        user_id='me'
+        meetings = client.meeting.list(user_id=user_id, type="upcoming_meetings")
+        for meeting in meetings:
+            print(meeting)
         
 
 if __name__ == "__main__":
@@ -20,4 +25,4 @@ if __name__ == "__main__":
     parser.add_argument("--zoom-api-key", dest="zoom_api_key", type=str, required=True)
     parser.add_argument("--zoom-api-secret", dest="zoom_api_secret", type=str, required=True)
     args = parser.parse_args()
-    authenticate_and_query(args.zoom_api_key, args.zoom_api_secret)
+    dza = DetectZoomAPI(args.zoom_api_key, args.zoom_api_secret)
