be8272464b79eea028a870dd14883259b7d007d4
Niraj Tolia <ntolia@users.noreply.github.com>
2022-04-12 18:21:18 -0700

    Add support for xbar

---
+++ xbar.2m.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#!/usr/bin/env /usr/local/bin/node
+# <bitbar.title>Busylight</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Niraj Tolia</bitbar.author>
+# <bitbar.author.github>ntolia</bitbar.author.github>
+# <bitbar.desc>Tweak busylight settings depending on meeting status</bitbar.desc>
+
+pythonenv="/usr/local/bin/python3"
+script="/Users/ntolia/src/busylight/controller.py"
+
+source /Users/ntolia/.bash_profile
+$pythonenv $script
